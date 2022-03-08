f793e88c0bc9029daff81df392d890e4fc3cdee0
hellosurbhi <messagesurbhi@gmail.com>
2022-03-08 13:26:00 -0500

    updated code post code pairing exercise

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="doggie-tinder">
+    <h2>Tinder yo dogs</h2>
+    <div class="doggie-profiles">
+      <div v-for="profile in profiles" :key="profile" class="doggie">
+        <img height="400" width="400" alt="individual-dog-picture" :src="profile">
+      </div>
+    </div>
+    <button @click="callProfile">></button>
   </div>
 </template>
 
@@ -34,25 +14,36 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+  },
+  data: function () {
+    return {
+          quantity: 1,
+          profiles: {},
+    }
+  },
+  created() {
+    this.callProfile()
+  },
+  // created() {
+  //   fetch(`https://dog.ceo/api/breeds/image/random/${this.quantity}`)
+  //   .then(response => response.json())
+  //   .then(data => this.profiles = { ...data.message})
+  //   },
+methods: {
+  callProfile() {
+    fetch(`https://dog.ceo/api/breeds/image/random/${this.quantity}`)
+    .then(response => response.json())
+    .then(data => this.profiles = { ...data.message})
+    },
+    showNextDog() {
+
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+
 </style>
